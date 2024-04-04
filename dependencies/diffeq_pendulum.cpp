d86a4da4fb4c964eb34d4d8c9accb2675e74a123
Connor Fricke <cd.fricke23@gmail.com>
2024-04-04 01:24:20 -0400

    i dont even remember what I did its 1 am

--- dependencies/diffeq_pendulum.cpp
+++ dependencies/diffeq_pendulum.cpp
@@ -1,5 +1,5 @@
 //  file: diffeq_pendulum.cpp
-// 
+//
 //  Program to solve the differential equation for a physical
 //   (chaotic) pendulum, as in Chapter 14 of the Landau/Paez text
 //
@@ -10,7 +10,7 @@
 //      02/10/04  original version, translated from diffeq_pendulum.c
 //      02/17/04  including direct piping to gnuplot (from gnuplot_i.c)
 //      01/30/06  put declarations and initializations together;
-//                 switched to <cmath> 
+//                 switched to <cmath>
 //      02/05/06  switched to GnuplotPipe class
 //      04/03/24  Connor Fricke: minor changes for adaptation to VisualizingChaos Project,
 //                cleaned up main(), cleaned up IO formatting
@@ -25,15 +25,15 @@
 //******************************************************************
 // include files
 #define _USE_MATH_DEFINES // for M_PI definition
-#include <iostream>    // note that .h is omitted
-#include <iomanip>    // note that .h is omitted
-#include <fstream>    // note that .h is omitted
+#include <iostream>       // note that .h is omitted
+#include <iomanip>        // note that .h is omitted
+#include <fstream>        // note that .h is omitted
 #include <string>
 #include <cstdlib>
-using namespace std;    // we need this when .h is omitted
+using namespace std; // we need this when .h is omitted
 #include <cmath>
-#include "diffeq_routines.h"  // diffeq routine prototypes
-#include "GnuplotPipe.h"  // direct piping
+#include "diffeq_routines.h" // diffeq routine prototypes
+#include "GnuplotPipe.h"     // direct piping
 
 // *** STRUCTURES ***
 // DEFAULT PARAMETERS FOR PENDULUM
@@ -46,6 +46,7 @@ struct pendulum_parameters
   double phi_ext = 0.0;     // phase angle for external force
   double theta0 = 0.8;      // initial (angular) position
   double theta_dot0 = 0.0;  // initial (angular) velocity
+  double T_ext = 2. * M_PI / omega_ext; // period for external frequency
 };
 
 // DEFAULT PARAMETERS USED FOR PLOTTING
@@ -61,30 +62,32 @@ struct plot_parameters
 };
 
 // *** FUNCTION PROTOTYPES ***
-double rhs (double t, double y[], int i, void *params_ptr);
-void queryParameters(pendulum_parameters &pendParams, plot_parameters &plotParams, double& T_ext, double& h);
+double rhs(double t, double y[], int i, void *params_ptr);
+void queryParameters(pendulum_parameters &pendParams, plot_parameters &plotParams, double &h);
 
 //*************************** main program ***************************
-int
-main (void)
+int main(void)
 {
-  const string FILENAME = "misc\\diffeq_pendulum.dat";  // filename for the output file
+  const string FILENAME = "misc\\diffeq_pendulum.dat"; // filename for the output file
+  GnuplotPipe myPipe;
 
-  const int N = 2;    // 2nd order equation --> 2 coupled 1st
-  double y_rk4[N];    // vector of y functions 
-  
+  const int N = 2; // 2nd order equation --> 2 coupled 1st
+  double y_rk4[N]; // vector of y functions
+
   // *** ASSIGN ALL PARAMETERS ***
-  void *rhs_params_ptr;             // void pointer passed to functions
-  pendulum_parameters pendParams;   // parameters for the pendulum behavior
+  void *rhs_params_ptr;           // void pointer passed to functions
+  pendulum_parameters pendParams; // parameters for the pendulum behavior
   plot_parameters plotParams;
-  double T_ext, h; // extraneous parameters that cannot belong to either structure
+  double h; // extraneous parameters that cannot belong to either structure
 
-  queryParameters(pendParams, plotParams, T_ext, h);
+// replaced while loop with goto statement for readability
+repeat:
+{
+  queryParameters(pendParams, plotParams, h);
 
   // *** PLOTTING STAGE ***
-  // declare a GnuplotPipe object and set some properties
-  GnuplotPipe myPipe;
-  myPipe.set_terminal("windows title 'does this work?' size 720,720");
+  // set some properties of the Gnuplot Pipe
+  myPipe.set_terminal("windows title 'Gnuplot: Visualizing Chaos' size 720,720");
   myPipe.set_title("Pendulum Phase Space");
   myPipe.set_xlabel("theta");
   myPipe.set_ylabel("theta dot");
@@ -116,73 +119,74 @@ main (void)
         << y_rk4[0] << "  " << y_rk4[1] << endl;
     myPipe.plot(pendParams.theta0, pendParams.theta_dot0);  // plot 1st point
     myPipe.plot2(pendParams.theta0, pendParams.theta_dot0); // plot 1st point
-    }
+  }
 
-    int point_count = 0;    // initialize point counter 
-    for (double t = plotParams.tmin; t <= plotParams.tmax; t += h)
+  int point_count = 0; // initialize point counter
+  for (double t = plotParams.tmin; t <= plotParams.tmax; t += h)
+  {
+    // find y(t+h) by a 4th order Runge-Kutta step
+    runge4(N, t, y_rk4, h, rhs, rhs_params_ptr);
+
+    if ((t >= plotParams.plot_min) & (t <= plotParams.plot_max))
     {
-      // find y(t+h) by a 4th order Runge-Kutta step 
-      runge4 (N, t, y_rk4, h, rhs, rhs_params_ptr);
+      point_count++; // increment point counter
 
-      if ((t >= plotParams.plot_min) & (t <= plotParams.plot_max))
-      {
-        point_count++;  // increment point counter 
+      double theta = y_rk4[0];     // current angle
+      double theta_dot = y_rk4[1]; // current angular velocity
 
-        double theta = y_rk4[0];       // current angle
-        double theta_dot = y_rk4[1];   // current angular velocity
-
-        if ((point_count % plotParams.plot_skip) == 0)
-        {    // plot every plot_skip points 
-          out << t + h << " " << scientific << setprecision (15)
-               << theta << " " << theta_dot << endl;
-                    // send points to gnuplot
-          myPipe.plot (theta, theta_dot);  
-        }
-        if ((point_count % plotParams.T_skip) == 0)
-        {    // plot every T_skip points 
-             // send points to gnuplot
-          myPipe.plot2 (theta, theta_dot);  
-        }
+      if ((point_count % plotParams.plot_skip) == 0)
+      { // plot every plot_skip points
+        out << t + h << " " << scientific << setprecision(15)
+            << theta << " " << theta_dot << endl;
+        // send points to gnuplot
+        myPipe.plot(theta, theta_dot);
       }
-    }  // end for loop over t
+      if ((point_count % plotParams.T_skip) == 0)
+      { // plot every T_skip points
+        // send points to gnuplot
+        myPipe.plot2(theta, theta_dot);
+      }
+    }
+  } // end for loop over t
 
-    out << endl;
-    out.close ();    // close the output file 
-    cout << "\n results added to diffeq_pendulum.dat\n\n";
+  out << endl;
+  out.close(); // close the output file
+  cout << "\n results added to diffeq_pendulum.dat\n\n";
+}
+  // prompt the user to avoid the window closing immediately
+  char AGAIN;
+  cout << "Again? Y/N\n>> ";
+  cin >> AGAIN;
+  if (AGAIN == 'Y' or AGAIN == 'y')
+    goto repeat;
 
-    // prompt the user to avoid the window closing immediately
-    string CONTINUE;
-    cout << "Enter Q/q to quit program.\n>> "; cin >> CONTINUE;
-    while (CONTINUE != "q" and CONTINUE != "Q")
-      cin >> CONTINUE;
+  cout << "Wait while the pipe is closed . . . " << flush;
+  myPipe.finish(); // close the pipe to gnuplot
+  cout << "Complete." << endl;
 
-    cout << "Wait while the pipe is closed . . . " << flush;
-    myPipe.finish ();  // close the pipe to gnuplot
-    cout << "Complete." << endl;
-
-  return (0);      // successful completion! 
+  return (0); // successful completion!
 }
 
 //*************************** rhs ***************************
 //
-//  * This is the function defining the i'th right hand side of 
+//  * This is the function defining the i'th right hand side of
 //     the diffential equations:
 //             dy[i]/dt = rhs(t,y[],i)
 //  * We take this from eqs. (14.5) through (14.7) in Landau/Paez
 //
 //*************************************************************
 double
-rhs (double t, double y[], int i, void *params_ptr)
+rhs(double t, double y[], int i, void *params_ptr)
 {
   // define local force parameters from passed structure
-  double omega0 = ((pendulum_parameters *) params_ptr)->omega0;
-  double alpha = ((pendulum_parameters *) params_ptr)->alpha;
-  double f_ext = ((pendulum_parameters *) params_ptr)->f_ext;
-  double omega_ext = ((pendulum_parameters *) params_ptr)->omega_ext;
-  double phi_ext = ((pendulum_parameters *) params_ptr)->phi_ext;
-  
+  double omega0 = ((pendulum_parameters *)params_ptr)->omega0;
+  double alpha = ((pendulum_parameters *)params_ptr)->alpha;
+  double f_ext = ((pendulum_parameters *)params_ptr)->f_ext;
+  double omega_ext = ((pendulum_parameters *)params_ptr)->omega_ext;
+  double phi_ext = ((pendulum_parameters *)params_ptr)->phi_ext;
+
   // External force
-  double F_ext = f_ext * cos (omega_ext * t + phi_ext);
+  double F_ext = f_ext * cos(omega_ext * t + phi_ext);
 
   if (i == 0)
   {
@@ -191,17 +195,28 @@ rhs (double t, double y[], int i, void *params_ptr)
 
   if (i == 1)
   {
-    return (-omega0 * omega0 * sin (y[0]) - alpha * y[1] + F_ext);
+    return (-omega0 * omega0 * sin(y[0]) - alpha * y[1] + F_ext);
   }
 
-  return (1);      // something's wrong if we get here 
+  return (1); // something's wrong if we get here
 }
 
-void queryParameters(pendulum_parameters& pendParams, plot_parameters& plotParams, double& T_ext, double& h)
+//******************* queryParameters *************************
+//
+//  * Originally part of main() function, moved to a separate
+//    task for readability. This function takes pass-by-reference
+//    parameters, most of which are stored between two structs.
+//  * The function reprompts the user for a value they'd like to
+//    change until they enter 0.
+//  * T_ext and h do not belong to either struct because they are
+//    calculated using values from the structs, though they could
+//    be added to the structs optionally
+//
+//*************************************************************
+void queryParameters(pendulum_parameters &pendParams, plot_parameters &plotParams, double &h)
 {
   // modify and return extraneous parameters that do not belong to either structure
-  T_ext = 2. * M_PI / pendParams.omega_ext; // period for external frequency
-  h = T_ext / double(plotParams.T_skip);
+  h = pendParams.T_ext / double(plotParams.T_skip);
 
   // PROMPT FOR ALL OTHER PARAMETERS
   int answer = 1;     // answer to parameter queries
@@ -247,8 +262,8 @@ void queryParameters(pendulum_parameters& pendParams, plot_parameters& plotParam
     case 4:
       cout << "enter w_ext:\n>> ";
       cin >> pendParams.omega_ext;
-      T_ext = 2. * M_PI / pendParams.omega_ext;
-      h = T_ext / 1000.; // set h according to external period
+      pendParams.T_ext = 2. * M_PI / pendParams.omega_ext;
+      h = pendParams.T_ext / 1000.; // set h according to external period
       break;
     case 5:
       cout << "enter phi_ext:\n>> ";
@@ -289,11 +304,11 @@ void queryParameters(pendulum_parameters& pendParams, plot_parameters& plotParam
     case 14:
       cout << "enter Gnuplot_delay (in msec):\n>> ";
       cin >> plotParams.plot_delay;
-      
+
     default:
       break;
     } // end switch answer
   }   // end answer while
-  
+
   return;
 }
